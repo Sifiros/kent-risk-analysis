73bbdf33e93f4f4f9172c599794104d863fc15d6
Benjamin Roux <benjamin2.roux@epitech.eu>
2019-06-13 19:36:20 +0200

    finalize front

--- dataset-web-harvester/src/HarvestingUnit.js
+++ dataset-web-harvester/src/HarvestingUnit.js
@@ -3,30 +3,39 @@ import Button from 'react-bootstrap/Button'
 import ReactJson from 'react-json-view'
 import './HarvestUnit.css'
 
+let CollectionState = {
+    IDLE: 0,
+    COLLECTED: 1,
+    SENT: 2
+}
+
 class HarvestingUnit extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            collectionStatus: false
+            collectionState: CollectionState.IDLE
         }
-        this.json = {"threeDSServerTransID": "8a880dc0-d2d2-4067-bcb1-b08d1690b26e", "doNotTrack": 1, "screenSize": "1920:1080", "plugins": ["Adblocks", "Google"], "position": {"lat": 95, "lng": 1.234525}, "browser": {"appName": "Netscape", "major": "67", "name": "Firefox", "version": "67.0"}, "cpu": {"architecture": "amd64"}, "os": {"name": "Windows", "version": "10"}}
+        this.json = {"doNotTrack": 1, "screenSize": "1920:1080", "plugins": ["Adblocks", "Google"], "position": {"lat": 95, "lng": 1.234525}, "browser": {"appName": "Netscape", "major": "67", "name": "Firefox", "version": "67.0"}, "cpu": {"architecture": "amd64"}, "os": {"name": "Windows", "version": "10"}}
     }
 
     onLaunchButtonClicked = () => {
         // TODO : START HARVESTING
         this.setState({
-            collectionStatus: true
+            collectionState: CollectionState.COLLECTED
         })
     }
 
     onSendButtonClicked = () => {
-
+        // TODO : PUSH DATA ON FIREBASE
+        this.setState({
+            collectionState: CollectionState.SENT
+        })
     }
 
     render() {
         return (
             <div className="HarvestUnitContainer">
-                {this.state.collectionStatus ? (
+                {this.state.collectionState === CollectionState.COLLECTED ? (
                     <div className="JsonContainer">
                         <h3>You will send these data :</h3>
                         <div className="JsonBox">
@@ -37,6 +46,12 @@ class HarvestingUnit extends Component {
                             onClick={this.onSendButtonClicked}>
                             Envoyer
                         </Button>
+                    </div>
+                ) : this.state.collectionState === CollectionState.SENT ? (
+                    <div className="ThanksContainer">
+                        <div className="ThanksBox">
+                            <h2>Thanks for your contribution</h2>
+                        </div>
                     </div>
                 ) : (
                     <Button 
