62a273bffe77591537d77010ea7f0947aff0a0d4
Benjamin Roux <benjamin2.roux@epitech.eu>
2019-06-14 16:57:15 +0200

    finalize dataset-web-harvester

--- dataset-web-harvester/src/Components/HarvestingUnit.js
+++ dataset-web-harvester/src/Components/HarvestingUnit.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
 import ReactJson from 'react-json-view'
+import CircularProgress from '@material-ui/core/CircularProgress';
 import HarvestedDataController from  '../Controllers/HarvestedDataController'
 import getAllInfo from '../harvester'
 import './HarvestUnit.css'
@@ -8,7 +9,8 @@ import './HarvestUnit.css'
 let CollectionState = {
     IDLE: 0,
     COLLECTION: 1,
-    SENT: 2
+    COLLECTED: 2,
+    SENT: 3
 }
 
 class HarvestingUnit extends Component {
@@ -21,6 +23,9 @@ class HarvestingUnit extends Component {
     }
 
     onLaunchButtonClicked = () => {
+        this.setState({
+            collectionState: CollectionState.COLLECTION
+        })
         getAllInfo().then(res => {
             console.log(res)
             // Replace potencial undefined fields by null to be able to post data to the db
@@ -28,9 +33,9 @@ class HarvestingUnit extends Component {
                 typeof value === 'undefined' ? null : value;
             let stringified = JSON.stringify(res, replacer)
             this.json = JSON.parse(stringified)
-            
+
             this.setState({
-                collectionState: CollectionState.COLLECTION
+                collectionState: CollectionState.COLLECTED
             })
         })
     }
@@ -46,22 +51,29 @@ class HarvestingUnit extends Component {
         return (
             <div className="HarvestUnitContainer">
                 {this.state.collectionState === CollectionState.COLLECTION ? (
-                    <div className="JsonContainer">
-                        <h3>You will send these data :</h3>
-                        <div className="JsonBox">
-                            <ReactJson src={this.json} />
+                    <div className="LoadingOverlayBoxContainer">
+                        <div className="LoadingOverlayBox">
+                            <h2>Gathering data, loading...</h2>
+                            <CircularProgress/>
                         </div>
-                        <Button 
-                            variant="primary"
-                            onClick={this.onSendButtonClicked}>
-                            Send
-                        </Button>
                     </div>
                 ) : this.state.collectionState === CollectionState.SENT ? (
                     <div className="ThanksContainer">
                         <div className="ThanksBox">
                             <h2>Thanks for your contribution</h2>
                         </div>
+                    </div>
+                ) : this.state.collectionState === CollectionState.COLLECTED ? (
+                    <div className="JsonContainer">
+                        <h3>You will send these data :</h3>
+                        <div className="JsonBox">
+                            <ReactJson src={this.json} collapseStringsAfterLength={40}/>
+                        </div>
+                        <Button 
+                            variant="primary"
+                            onClick={this.onSendButtonClicked}>
+                            Send
+                        </Button>
                     </div>
                 ) : (
                     <Button 
