d06d0df7e1473a23404fee5753cf9453bbac5520
Benjamin Roux <benjamin2.roux@epitech.eu>
2019-06-17 15:45:01 +0200

    add checkboxs

--- dataset-web-harvester/src/Components/HarvestingUnit.js
+++ dataset-web-harvester/src/Components/HarvestingUnit.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 import ReactJson from 'react-json-view'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import HarvestedDataController from  '../Controllers/HarvestedDataController'
@@ -17,7 +18,11 @@ class HarvestingUnit extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            collectionState: CollectionState.IDLE
+            collectionState: CollectionState.IDLE,
+            understanding: false,
+            voluntary: false,
+            gdpr: false,
+            isNotAbleToSend: true
         }
         this.harvestedDataController = new HarvestedDataController()
     }
@@ -44,6 +49,24 @@ class HarvestingUnit extends Component {
         this.harvestedDataController.createHarvestedData(this.json)
         this.setState({
             collectionState: CollectionState.SENT
+        })
+    }
+
+    onUnderstandingCheckboxChanged = (value) => {
+        this.setState({
+            understanding: value.target.checked
+        })
+    }
+
+    onVoluntaryCheckboxChanged = (value) => {
+        this.setState({
+            voluntary: value.target.checked
+        })
+    }
+
+    onGdprCheckboxChanged = (value) => {
+        this.setState({
+            gdpr: value.target.checked
         })
     }
 
@@ -76,12 +99,20 @@ class HarvestingUnit extends Component {
                         </Button>
                     </div>
                 ) : (
-                    <Button 
-                        variant="outline-danger" 
-                        size="lg"
-                        onClick={this.onLaunchButtonClicked}>
-                        Launch Data Collection
-                    </Button>
+                    <div className="FormContainer">
+                        <Form.Group controlId="UnderstandingCheckbox">
+                            <Form.Check value={this.state.understanding} type="checkbox" label="I confirm I have read and understand the information for the above study" onChange={this.onUnderstandingCheckboxChanged}/>
+                            <Form.Check value={this.state.voluntary} type="checkbox" label="I understand that my participation is voluntary and that I am free to withdraw at any time without giving any reason" onChange={this.onVoluntaryCheckboxChanged}/>
+                            <Form.Check value={this.state.gdpr} type="checkbox" label="I understand that my responses will be anonymised before analysis. I give permission for members of the research team to have access to my anonymised responses." onChange={this.onGdprCheckboxChanged}/>
+                        </Form.Group>
+                        <Button 
+                            variant="outline-danger" 
+                            size="lg"
+                            disabled={!(this.state.voluntary === true && this.state.understanding === true && this.state.gdpr === true)}
+                            onClick={this.onLaunchButtonClicked}>
+                            Launch Data Collection
+                        </Button>
+                    </div>
                 )}
             </div>
         )
