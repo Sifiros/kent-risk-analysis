ce016bb4cf0a78a23c6f4cbfee9f9127b13ae427
tinmar <martin.caussanel@epitech.eu>
2019-06-20 17:17:57 +0200

    Merchant dockerization & integration to docker-compose

--- Merchant/config.js
+++ Merchant/config.js
@@ -1,17 +1,27 @@
 let port = () => {
-    return 4242
+    return process.env.SERVER_PORT || 4242
+}
+
+let serverIp  = () => {
+    return process.env.SERVER_IP || 'localhost'
 }
 
 let origin = () => {
-    return "http://localhost:4242"
+    return process.env.PUBLIC_URL || "http://localhost:4242"
+}
+
+let internalNetworkUrl = () => {
+    return process.env.INTERNAL_NETWORK_URL || "http://merchant:4242"
 }
 
 let acsAddr = () => {
-    return "http://localhost:8484"
+    return process.env.ACS_PUBLIC_URL || "http://localhost:8484"
 }
 
 module.exports = {
     port,
+    serverIp,
     origin,
+    internalNetworkUrl,
     acsAddr
 }
